d30e9bce8eed73c254851e298c2a2954a54ee706
Riki Medan <rhehnsi@gmail.com>
2026-05-05 20:19:07 +0300

    CRAET BOARD

--- Boad.js
+++ Boad.js
@@ -3,13 +3,17 @@ let sequence=[];
 let userInput=[];
 let lavel=0;
 let id=1;
+const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "cyan", "lime"];
 
 function creatBoard(){
     for(let i =0;i<size_b;i++){
     const cell= document.createElement("div");
     cell.index=i+1;
     cell.class ="cell ";
-    document.getElementById("board").appendChild(cell)
-        console.log(`creat data ${i+1}`);
+    document.getElementById("board").appendChild(cell);
+    cell.style.backgroundColor = colors[i];
+
+    console.log(`creat data ${i+1} and color ${colors[i]}`);
+
 }}
 creatBoard()
